7b90a38defabf8592a0497a9747c83187529eb35
omergal99 <omergal99gm@gmail.com>
2020-02-01 15:43:51 +0200

    Updates

---
+++ precache-manifest.e5ee966cba6d10ee0853766416e4f9f9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f102aa780c2635b572048c6f6bfcd71",
+    "url": "/drag-drop-grid/index.html"
+  },
+  {
+    "revision": "ddf360795a254191132a",
+    "url": "/drag-drop-grid/static/css/main.8cd0dd8b.chunk.css"
+  },
+  {
+    "revision": "1570e3c7a5f3f04f46f0",
+    "url": "/drag-drop-grid/static/js/2.bf3271e2.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/drag-drop-grid/static/js/2.bf3271e2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddf360795a254191132a",
+    "url": "/drag-drop-grid/static/js/main.2921cea6.chunk.js"
+  },
+  {
+    "revision": "bee26f4a95227884b77e",
+    "url": "/drag-drop-grid/static/js/runtime-main.43fe9786.js"
+  },
+  {
+    "revision": "7e0ee8c45d872446649aacd3ce96e1f7",
+    "url": "/drag-drop-grid/static/media/SourceSansPro-Bold.7e0ee8c4.ttf"
+  },
+  {
+    "revision": "3ba9836fde035d78b4c3f58db4a763ba",
+    "url": "/drag-drop-grid/static/media/SourceSansPro-Light.3ba9836f.ttf"
+  },
+  {
+    "revision": "3f482321becfaa40fcb19825fa717859",
+    "url": "/drag-drop-grid/static/media/SourceSansPro-Regular.3f482321.ttf"
+  }
+]);
